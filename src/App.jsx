83c8e56c937a0191cbee6ca8371f8775f576dcab
SalvatoreCodes <goniputra1@gmail.com>
2023-03-08 20:12:21 +0700

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import "./styles/general.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="hero">
+        <div className="hero--navigations">
+          <ul>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Join Us</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Gallery</a>
+            </li>
+          </ul>
+        </div>
+        <h1>Welcome To PPLG</h1>
+        <div className="hero--down-arrow">
+          <h2>Learn More</h2>
+          <img src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/1e90ff/external-down-arrow-miscellaneous-kiranshastry-solid-kiranshastry.png" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
